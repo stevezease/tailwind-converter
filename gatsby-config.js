6dbe429815694c14b7f660534f4527699402ac0f
Steven Jin <stevenjin@Stevens-MBP-3.fios-router.home>
2020-04-18 20:59:13 -0400

    secondary commit for initial draft

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    siteMetadata: {
+        title: `TailwindCSS Converter`,
+        description: `Easily convert CSS to Tailwind with this online converter. Paste in your Css and the tool takes care of the rest!`,
+        author: `@StevenJin`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/tailwind.ico`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-plugin-postcss`,
+            options: {
+                postCssPlugins: [require('tailwindcss')],
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+};
