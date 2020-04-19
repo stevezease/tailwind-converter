44e8f49e6caa7c5c175386222339eb0e17c7a539
Steven Jin <stevenjin@Stevens-MBP-3.fios-router.home>
2020-04-18 21:02:58 -0400

    change icon image

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/tailwind.ico`, // This path is relative to the root of the site.
+                icon: `src/images/tailwind.png`, // This path is relative to the root of the site.
             },
         },
         {
