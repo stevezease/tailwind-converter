1a224e61cc8df53af3665b2a0fb497928bba030d
Abhishek Vishwakarma <52538014+abhigdrv@users.noreply.github.com>
2023-03-01 15:43:45 +0530

    feat: privacy policy added

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,134 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="Privacy Policy" />
+    <h1>Privacy Policy</h1>
+    <p>
+    This Privacy Policy governs any personal information Css To Tailwind Inc. (“Css To Tailwind,” “we,” “us,” or “our”) collects, how we use and share that data, and your choices concerning our data practices. Through its website (the “Site”), Css To Tailwind provides sales and marketing information about specialized data privacy vaults which store and utilize sensitive data safely and securely (such information, together with the Site, the “Service”). 
+
+The Service is directed towards our customers and potential customers. It should not be used for personal, family, or household purposes. We treat all personal information covered by this Privacy Policy as pertaining to individuals acting as business representatives, rather than in their personal capacity.
+
+Before using the Service or submitting any personal information to Css To Tailwind, please review this Privacy Policy carefully and contact us if you have any questions. By using the Service, you agree to the practices described in this Privacy Policy. If you do not agree to this Privacy Policy, please do not access the Site or otherwise use the Service.
+
+1. PERSONAL INFORMATION WE COLLECT
+
+Personal Information You Provide: We collect personal information from you when you request a demo, participate in one of our events or conferences, contact us, visit the Site, our branded social media pages, or our offices, use the Service, apply for jobs, send communications to or receive communications from us, or otherwise provide personal information to us. The personal information we collect varies depending on what you choose to share, but it may include your name and email address.
+
+We also have pages on social media sites like Twitter and LinkedIn (“Social Media Pages”). When you interact with our Social Media Pages, we will collect personal information that you elect to provide to us, such as your contact details. In addition, the companies that host our Social Media Pages may provide us with aggregate information and analytics regarding the use of our Social Media Pages.
+
+Internet Activity Data: When you visit, use, and interact with the Service, we may receive certain information about your visit, use, or interactions. For example, we may monitor the number of people that visit the Service, peak hours of visits, which page(s) are visited, the domains our visitors come from (e.g., google.com, yahoo.com, etc.), and which browsers people use to access the Service (e.g., Chrome, Firefox, Microsoft Internet Explorer, etc.), broad geographical information, and navigation pattern. In particular, the following information is created and automatically logged in our systems: 
+
+Log Data: Information that your browser automatically sends whenever you visit the Site. Log Data includes your Internet Protocol address, browser type and settings, the date and time of your request, and how you interacted with the Site. 
+Cookies Data: Please see the “Cookies” section below to learn more about how we use cookies.
+Device Data: Includes name of the device, operating system, and browser you are using. Information collected may depend on the type of device you use and its settings. 
+Usage Data: We collect information about how you use our Service, such as the types of content that you view or engage with, the features you use, the actions you take, and the time, frequency, and duration of your activities.
+Location Data: We derive a rough estimate of your location from your IP address. 
+Email Open/Click Data: We use pixels in our email campaigns that allow us to collect your email and IP address as well as the date and time you open an email or click on any links in the email.
+Personal Information We Process on Behalf of Our Business Customers: In order to provide our data privacy vaults, we collect sensitive data and personal information from our customers. We process that data pursuant to our agreements with our business customers. We have no direct relationship with the individuals whose personal information we process in the course of providing our data privacy vaults to our customers. If you are such an individual and have questions about the processing of your personal information, please contact the business customer that you interact with directly.
+
+Cookies: We use cookies to operate and administer our Site, gather usage data on our Site, and improve your experience on it. A “cookie” is a piece of information sent to your browser by a website you visit. Cookies can be stored on your computer for different periods of time. Some cookies expire after a certain amount of time, or upon logging out (session cookies), others survive after your browser is closed until a defined expiration date set in the cookie (as determined by the third party placing it), and help recognize your computer when you open your browser and browse the Internet again (persistent cookies). For more details on cookies please visit All About Cookies.
+
+On most web browsers, you will find a “help” section on the toolbar. Please refer to this section for information on how to receive a notification when you are receiving a new cookie and how to turn cookies off. 
+
+Please note that if you limit the ability of websites to set cookies, you may be unable to access certain parts of the Site and you may not be able to benefit from the full functionality of the Site.
+
+Advertising networks may use cookies to collect personal information. Most advertising networks offer you a way to opt out of targeted advertising. If you would like to find out more information, please visit the Network Advertising Initiative’s online resources at http://www.networkadvertising.org and follow the opt-out instructions there.
+
+If you access the Site on your mobile device, you may not be able to control tracking technologies through the settings. 
+
+Analytics: We use Google Analytics, a web analytics service provided by Google, Inc. (“Google”). Google Analytics uses cookies to help us analyze how users use the Site and enhance your experience when you use the Site. For more information on how Google uses this information, click here.
+
+Online Tracking and Do Not Track Signals: We and our third party service providers may use cookies, pixels, or other tracking technologies to collect information about your browsing activities over time and across different websites following your use of the Site and use that information to send targeted advertisements. Our Site currently does not respond to “Do Not Track” (“DNT”) signals and operates as described in this Privacy Policy whether or not a DNT signal is received. If we do respond to DNT signals in the future, we will update this Privacy Policy to describe how we do so. 
+
+2. HOW WE USE PERSONAL INFORMATION
+
+We may use personal information for the following purposes:
+
+To provide the Service and conduct our business;
+To perform market research;
+To respond to your inquiries, comments, feedback, or questions;
+To send administrative information to you, for example, information regarding the Service and changes to our terms, conditions, and policies;
+To analyze how you interact with our Service;
+To maintain and improve the Service;
+To develop new products and services;
+To prevent fraud, criminal activity, or misuses of our Service, and to ensure the security of our IT systems, architecture, and networks; and
+To comply with legal obligations and legal process and to protect our rights, privacy, safety, or property, and/or that of our affiliates, you, or other third parties.
+Aggregated Information. We may aggregate personal information and use the aggregated information to analyze the effectiveness of our Service, to improve and add features to our Service, and for other similar purposes. In addition, from time to time, we may analyze the general behavior and characteristics of users of our Service and share aggregated information like general user statistics with prospective business partners. We may collect aggregated information through the Service, through cookies, and through other means described in this Privacy Policy.
+
+Marketing. We may use your personal information to contact you to tell you about products or services we believe may be of interest to you. For instance, if you elect to provide your email, we may use that information to send you special offers. You may opt out of receiving emails by following the instructions contained in each promotional email we send you. In addition, if at any time you do not wish to receive future marketing communications, you may contact us. If you unsubscribe from our marketing lists, you will no longer receive marketing communications but we will continue to contact you regarding other administrative matters and to respond to your requests. We may also use your personal information to analyze and improve our marketing campaigns.
+
+3. SHARING AND DISCLOSURE OF PERSONAL INFORMATION
+
+In certain circumstances we may share the categories of personal information described above without further notice to you, unless required by the law, with the following categories of third parties:
+
+Vendors and Service Providers: To assist us in meeting business operations needs and to perform certain services and functions, we may share personal information with vendors and service providers, including providers of hosting services, cloud services, and other information technology services providers, email communication software and email newsletter services, advertising and marketing services, payment processors, customer relationship management and customer support services, and analytics services. Pursuant to our instructions, these parties will access, process, or store personal information in the course of performing their duties to us. 
+Business Transfers: If we are involved in a merger, acquisition, financing due diligence, reorganization, bankruptcy, receivership, sale of all or a portion of our assets, or transition of service to another provider (collectively a “Transaction”), your personal information and other information may be shared in the diligence process with counterparties and others assisting with the Transaction and transferred to a successor or affiliate as part of that Transaction along with other assets.
+Legal Requirements: If required to do so by law or in the good faith belief that such action is necessary to (i) comply with a legal obligation, including to meet national security or law enforcement requirements, (ii) protect and defend our rights or property, (iii) prevent fraud, (iv) act in urgent circumstances to protect the personal safety of users of the Service, or the public, or (v) protect against legal liability.
+‍
+4. PRIVACY RIGHTS
+
+Where provided for by law and subject to any applicable exceptions, you may have the right:
+
+To know the categories of personal information that Css To Tailwind has collected about you, the business purpose for collecting your personal information, and the categories of sources from which the personal information was collected;
+To access the specific pieces of personal information that Css To Tailwind has collected about you;
+To know whether Css To Tailwind has disclosed your personal information for business purposes, the categories of personal information so disclosed, and the categories of third parties to whom we have disclosed your personal information;
+To have Css To Tailwind, under certain circumstances, delete your personal information; 
+To instruct businesses that sell personal information to stop doing so – Css To Tailwind, however, does not sell personal information; and
+To be free from discrimination related to the exercise of these rights.
+If you would like to exercise any or all of these rights, you may do so by contacting us. After we receive your request, we may request additional information from you to verify your identity. Your authorized agent may submit requests in the same manner, although we may require the agent to present signed written permission to act on your behalf, and you may also be required to independently verify your identity with us and confirm that you have provided the agent permission to submit the request.
+
+Please contact us with questions or to request access to an alternative format of this Privacy Policy.
+
+5. GDPR
+
+VeraSafe has been appointed as Css To Tailwind's representative in the European Union for data protection matters, pursuant to Article 27 of the General Data Protection Regulation of the European Union. If you are in the European Economic Area, VeraSafe can be contacted in addition to privacy@Css To Tailwind.com, only on matters related to the processing of personal data. To make such an inquiry, please contact VeraSafe using this contact form or via telephone at: +420 228 881 031.
+
+Alternatively, VeraSafe can be contacted at:
+
+VeraSafe Ireland Ltd.
+Unit 3D North Point House
+North Point Business Park
+New Mallow Road
+Cork T23AT2P
+Ireland
+
+6. CHILDREN
+
+Our Service is not directed to children who are under the age of 16. Css To Tailwind does not knowingly collect personal information from children under the age of 16. If you have reason to believe that a child under the age of 16 has provided personal information to Css To Tailwind through the Service please contact us and we will endeavor to delete that information from our databases.
+
+7. LINKS TO OTHER WEBSITES
+
+The Service may contain links to other websites not operated or controlled by Css To Tailwind, including social media services (“Third Party Sites”). The information that you share with Third Party Sites will be governed by the specific privacy policies and terms of service of the Third Party Sites and not by this Privacy Policy. By providing these links we do not imply that we endorse or have reviewed these sites. Please contact the Third Party Sites directly for information on their privacy practices and policies. 
+
+8. SECURITY
+
+You use the Service at your own risk. We implement commercially reasonable technical, administrative, and organizational measures to protect personal information both online and offline from loss, misuse, and unauthorized access, disclosure, alteration, or destruction. However, no Internet or e-mail transmission is ever fully secure or error free. In particular, e-mail sent to or from us may not be secure. Therefore, you should take special care in deciding what information you send to us via the Service or e-mail. Please keep this in mind when disclosing any personal information to Css To Tailwind via the Internet. In addition, we are not responsible for circumvention of any privacy settings or security measures contained on the Service, or third party websites.
+
+9. INTERNATIONAL USERS
+
+By using our Service, you understand and acknowledge that your personal information will be transferred from your location to our facilities and servers in the United States, where data protection laws may differ from those in your jurisdiction. 
+
+10. JOB APPLICANTS 
+
+When you apply for a job at Css To Tailwind, we collect the information that you provide to us in connection with your job application. This includes business and personal contact information, professional credentials and skills, educational and work history, and other information that may be included in a resume. This may also include diversity information that you voluntarily provide. We use this information to facilitate our recruitment activities and process employment applications, such as evaluating candidates and monitoring recruitment statistics. We may also use and disclose this information (a) to improve the administration of the Site, (b) as otherwise necessary to comply with relevant laws, (c) to respond to subpoenas or warrants served on Css To Tailwind, and (d) to protect and defend the rights or property of Css To Tailwind or others.
+
+11. YOUR CHOICES
+
+In certain circumstances providing personal information is optional. However, if you choose not to provide personal information that is needed to use some features of our Service, you may be unable to use those features. You can also contact us to ask us to update or correct your personal information.
+
+12. CHANGES TO THE PRIVACY POLICY
+
+The Service and our business may change from time to time. As a result we may change this Privacy Policy at any time. When we do we will post an updated version on this page, unless another type of notice is required by the applicable law. By continuing to use our Service or providing us with personal information after we have posted an updated Privacy Policy, or notified you by other means if applicable, you consent to the revised Privacy Policy and practices described in it.
+
+13. CONTACT US
+
+If you have any questions about our Privacy Policy or information practices, please feel free to contact us at our designated request address: 
+    </p>
+  </Layout>
+)
+
+export default NotFoundPage
